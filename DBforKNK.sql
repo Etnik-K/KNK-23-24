353042f0ecc14fbc11e069a51610b1881927c9bb
Gresa-Hasani <gresahasani16@gmail.com>
2024-05-10 20:38:14 +0200

    update database

--- DBforKNK.sql
+++ DBforKNK.sql
@@ -5,30 +5,56 @@ drop table users;
 select * from users;
 
 create table faculties (
-                           faculty_id INT AUTO_INCREMENT PRIMARY KEY,
-                           faculty_name VARCHAR(100) NOT NULL
+    faculty_id INT AUTO_INCREMENT PRIMARY KEY,
+    faculty_name VARCHAR(100) NOT NULL
+);
+drop table users;
+create table users (
+	id integer primary key auto_increment,
+    firstName nvarchar(30),
+    lastName nvarchar(50),
+    email nvarchar(300),
+    salt nvarchar(100),
+    passwordHash nvarchar(500),
+    user_type ENUM('admin', 'professor', 'student') NOT NULL,
+    faculty_id INT,
+    is_approved BOOLEAN DEFAULT FALSE
 );
 
-create table users (
-                       id integer primary key auto_increment,
-                       firstName nvarchar(30),
-                       lastName nvarchar(50),
-                       email nvarchar(100),
-                       salt nvarchar(100),
-                       passwordHash nvarchar(500),
-                       user_type ENUM('admin', 'professor', 'student') NOT NULL,
-                       faculty_id INT,
-                       is_approved BOOLEAN DEFAULT FALSE
+CREATE TABLE profesor (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100)
 );
-create table schedules (
-                           schedule_id INT AUTO_INCREMENT PRIMARY KEY,
-                           faculty_id INT,
-                           course_name VARCHAR(100) NOT NULL,
-                           professor_id INT,
-                           day_of_week ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday') NOT NULL,
-                           start_time TIME NOT NULL,
-                           end_time TIME NOT NULL,
-                           CONSTRAINT fk_faculty FOREIGN KEY (faculty_id) REFERENCES faculties(faculty_id),
-                           CONSTRAINT fk_professor FOREIGN KEY (professor_id) REFERENCES users(id)
+
+CREATE TABLE lenda (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    lenda_name VARCHAR(100),
+    has_lab BOOLEAN
 );
--- profesori table, lenda table edhe salla table
+
+CREATE TABLE salla (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    salla_name VARCHAR(100),
+    capacity INT
+);
+
+CREATE TABLE Class (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    class_name VARCHAR(100),
+    profesor_id INT,
+    lenda_id INT,
+    salla_id INT,
+    time_slot_id INT,
+    FOREIGN KEY (profesor_id) REFERENCES profesor(id),
+    FOREIGN KEY (lenda_id) REFERENCES lenda(id),
+    FOREIGN KEY (salla_id) REFERENCES salla(id),
+    FOREIGN KEY (time_slot_id) REFERENCES TimeSlot(id)
+);
+
+CREATE TABLE TimeSlot (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    day_of_week VARCHAR(20),
+    start_time TIME,
+    end_time TIME
+);
+ -- profesori table, lenda table edhe salla table
